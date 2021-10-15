1a2d026de5115b51e669cfbaeb07686d26eb1555
hangnguyen81 <hangntt@gmail.com>
2021-10-15 22:16:52 +0300

    loading spinner

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,24 @@
 import { Link as ReachRouterLink } from 'react-router-dom';
-import { Background, Container, Logo, ButtonLink, Select, Option, Wrap } from './styles/header';
+import { useState } from 'react';
+import { Background, 
+        Container, 
+        Logo, 
+        ButtonLink, 
+        Select, 
+        Option, 
+        Wrap,
+        Group,
+        Text,
+        Link,
+        Feature,
+        PlayButton, 
+        FeatureCallOut,
+        Search,
+        SearchIcon,
+        SearchInput,
+        Profile,
+        Picture,
+        Dropdown } from './styles/header';
 
 export default function Header({ bg = true, children, ...restProps }) {
     return bg ? <Background {...restProps}>{children}</Background> : children;
@@ -31,4 +50,62 @@ Header.LangOption = function HeaderLangOption({children, ...restProps}){
 
 Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
     return <ButtonLink {...restProps}>{children}</ButtonLink>;
+}
+
+Header.Group = function HeaderGroup({ children, ...restProps }) {
+    return <Group {...restProps}>{children}</Group>;
+}
+
+Header.Text = function HeaderText({ children, ...restProps }) {
+    return <Text {...restProps}>{children}</Text>;
+}
+
+Header.Link = function HeaderLink({ children, ...restProps }) {
+    return <Link {...restProps}>{children}</Link>;
+}
+
+Header.Feature = function HeaderFeature({ children, ...restProps }) {
+    return <Feature {...restProps}>{children}</Feature>
+}
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({ children, ...restProps }) {
+    return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>
+}
+
+Header.PlayButton = function HeaderPlayButton({ children, ...restProps }) {
+    return <PlayButton {...restProps}>{children}</PlayButton>
+}
+
+Header.Search = function HeaderSearch({ searchTerm, setSearchTerm, ...restProps }) {
+    const [searchActive, setSearchActive] = useState(false);
+    
+    return (
+        <Search {...restProps}>
+            <SearchIcon onClick={() => setSearchActive(!searchActive)}>
+                <img src="/images/icons/search.png" alt="Search" />
+            </SearchIcon>
+            <SearchInput
+                value={searchTerm}
+                onChange={({ target }) => setSearchTerm(target.value)}
+                placeholder="Search films and series"
+                active={searchActive}
+            />
+        </Search>
+    )
+}
+
+Header.Profile = function HeaderProfile({children, ...restProps}){
+    return (
+        <Profile {...restProps}>
+            {children}
+        </Profile>
+    )
+}
+
+Header.Picture = function HeaderPicture({ src, ...restProps }) {
+    return <Picture {...restProps} src={`/images/users/${src}.png`} />;
+}
+
+Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
+    return <Dropdown {...restProps}>{children}</Dropdown>;
 }
--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 import { Link as ReachRouterLink } from 'react-router-dom';
 
+
 export const Background = styled.section`
     display: flex;
     flex-direction: column;
     background: url(${({ src }) => (src ? `../images/misc/${src}.jpg` : '../images/misc/home-bg.jpg')});
-
+    background-size: cover;
     @media (max-width: 1100px) {
         ${({ dontShowOnSmallViewPort }) => dontShowOnSmallViewPort && `background: none;`}
     }
@@ -78,19 +79,166 @@ export const Wrap = styled.div`
 `
 
 export const Select = styled.select`
-    opacity: 0.5;
+ 
     border: 1px solid white;
     padding: .2rem .5rem;
-    color: black;
+    color: white;
     width: 120px;
     height: fit-content;
     font-size: 1rem;
     padding: 6px 15px;
     margin-right: 1rem;
     border-radius: 3px;
+    background-color: transparent;
 `
 
 export const Option = styled.option`
-    
+    color: black;
 
 `
+
+export const Group = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+export const Text = styled.p`
+    color: white;
+    font-size: 22px;
+    line-height: normal;
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
+`;
+
+export const Feature = styled(Container)`
+    padding: 150px 0 500px 0;
+    flex-direction: column;
+    align-items: normal;
+    width: 50%;
+    
+    @media (max-width: 1100px) {
+        display: none;
+    }
+`;
+
+export const FeatureCallOut = styled.h2`
+    color: white;
+    font-size: 50px;
+    line-height: normal;
+    font-weight: bold;
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
+    margin: 0;
+`;
+
+export const PlayButton = styled.button`
+    box-shadow: 0 0.6vw 1vw -0.4vw rgba(0, 0, 0, 0.35);
+    background-color: #e6e6e6;
+    color: #000;
+    border-width: 0;
+    padding: 10px 20px;
+    border-radius: 5px;
+    max-width: 130px;
+    font-weight: bold;
+    font-size: 20px;
+    margin-top: 10px;
+    cursor: pointer;
+    transition: background-color: 0.5s ease;
+    
+    &:hover {
+        background: #ff1e1e;
+        color: white;
+    }
+`;
+
+export const Search = styled.div`
+    display: flex;
+    align-items: center;
+    
+    @media (max-width: 700px) {
+        display: none;
+    }
+`;
+
+export const SearchIcon = styled.button`
+    cursor: pointer;
+    background-color: transparent;
+    border: 0;
+    
+    img {
+        filter: brightness(0) invert(1);
+        width: 16px;
+    }
+`
+
+export const SearchInput = styled.input`
+    background-color: #444459;
+    color: white;
+    border: 1px solid white;
+    transition: width 0.5s;
+    height: 30px;
+    font-size: 14px;
+    margin-left: ${({ active }) => (active === true ? '10px' : '0')};
+    padding: ${({ active }) => (active === true ? '10px' : '0')};
+    opacity: ${({ active }) => (active === true ? '1' : '0')};
+    width: ${({ active }) => (active === true ? '200px' : '0px')};
+`;
+
+export const Picture = styled.button`
+    background: url(${({ src }) => src});
+    background-size: contain;
+    border: 0;
+    width: 32px;
+    height: 32px;
+    cursor: pointer;
+`;
+
+export const Dropdown = styled.div`
+    display: none;
+    position: absolute;
+    background-color: black;
+    padding: 10px;
+    width: 100px;
+    top: 32px;
+    right: 10px;
+    
+    ${Group}:last-of-type ${Link} {
+        cursor: pointer;
+    }
+    
+    ${Group} {
+        margin-bottom: 10px;
+        
+        &:last-of-type {
+            margin-bottom: 0;
+        }
+        
+        ${Link}, ${Picture} {
+            cursor: default;
+        }
+    }
+    
+    button {
+        margin-right: 10px;
+    }
+    
+    p {
+        font-size: 12px;
+        margin-bottom: 0;
+        margin-top: 0;
+    }
+`;
+
+export const Profile = styled.div`
+    display: flex;
+    align-items: center;
+    margin-left: 20px;
+    position: relative;
+    
+    button {
+        cursor: pointer;
+    }
+    
+    &:hover > ${Dropdown} {
+        display: flex;
+        flex-direction: column;
+    }
+`;
